10050806c856fc34b9a7eef0f9af7787ba626a94
kggitacc <72395176+kggitacc@users.noreply.github.com>
2022-04-27 22:39:18 +1000

    main gui + dummy classes
    Big update on gui implementing stub classes and initial actionlisteners. Some stub classes + javadoc

---
+++ src/AS1/GUI/DummyClasses.java
@@ -0,0 +1,53 @@
+package AS1.GUI;
+
+/**
+ * Methods for the gui to call
+ */
+public class DummyClasses {
+
+    /**
+     * Generates the window to draw the maze manually with window height and width, along with an Autoplaced image
+     * @param height
+     * @param width
+     * @param AutoplaceImage
+     */
+    public void Draw(int height, int width, boolean AutoplaceImage){
+
+
+
+    }
+
+    /**
+     * Generates a auto generated maze automatically using an algorithm
+     * with window height and width, along with an Autoplaced image
+     * @param height
+     * @param width
+     * @param AutoplaceImage
+     */
+    public void Generate(int height, int width, boolean AutoplaceImage){
+
+    }
+
+    /**
+     * Checks if the maze that is currently being drawn is solvable and displays a path if true
+     */
+    public void CheckPath(){
+
+    }
+
+
+    /**
+     * Saves the maze that is currently open along with the mazes name and author
+     * @param MazeName
+     * @param Author
+     */
+    public void SaveMaze(String MazeName, String Author){
+
+    }
+
+
+
+
+
+
+}
--- src/AS1/GUI/MainGUI.java
+++ src/AS1/GUI/MainGUI.java
@@ -2,9 +2,13 @@ package AS1.GUI;
 
 import AS1.Maze.Maze;
 import AS1.Maze.MazeCell;
+import jdk.jshell.execution.JdiExecutionControlProvider;
 
 import javax.swing.*;
+import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainGUI {
     JFrame ParentFrame;
@@ -29,24 +33,153 @@ public class MainGUI {
 
     }
 
-    public void HomeGUI(){
+    DummyClasses buttoncalls = new DummyClasses();
+    public void HomeGUI() {
+
+
+
+            ActionListener drawbutton = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //calls method from dummy classes to draw
+
+                }
+            };
+            ActionListener generatebutton = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //calls method from dummy classes to auto generate
+
+                }
+            };
+            ActionListener checkbutton = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            };
+            ActionListener savebutton = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //calls method from dummy classes to save maze
+
+                }
+            };
+            ActionListener generatepath = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //calls method from dummy classes to check path
+                }
+            };
+            ActionListener placeimage = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //calls method from dummy classes to place image
+                }
+            };
+            ActionListener autoplace = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                   //calls method from dummy classes to auto place image
+                }
+            };
+
+
 
         ParentFrame = new JFrame("Mazebuilder3000");
         ParentFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JTabbedPane pane=new JTabbedPane();
+
+
+        //Gui for drawing a maze from scratch
         JPanel MfromScratch = new JPanel();
 
-        MfromScratch.add(new JButton("Draw Maze!"));
+        JButton Draw = new JButton("Draw Maze!");
+        Draw.addActionListener(drawbutton);
+        MfromScratch.add(Draw);
+
         MfromScratch.add(new JLabel("Please Enter Height"));
         MfromScratch.add(new JTextField(2));
         MfromScratch.add(new JLabel("Please Enter Width"));
         MfromScratch.add(new JTextField(2));
-        MfromScratch.add(new JButton("Check Maze Path"));
+
+        JButton Path = new JButton("Check Maze Path");
+        Path.addActionListener(generatepath);
+        MfromScratch.add(Path);
+
         MfromScratch.add(new JTextField("Maze Name"));
         MfromScratch.add(new JTextField("Author Name"));
-        MfromScratch.add(new JRadioButton("Auto place logo"));
-        MfromScratch.add(new JButton("Save Maze"));
+        JRadioButton aLogo = new JRadioButton("Auto place logo");
+        aLogo.addActionListener(autoplace);
+        MfromScratch.add(aLogo);
+
+        JButton placeLogo = new JButton("Place Image");
+        placeLogo.addActionListener(placeimage);
+        MfromScratch.add(placeLogo);
+
+        JButton saveMaze = new JButton("Save Maze");
+        saveMaze.addActionListener(savebutton);
+        MfromScratch.add(saveMaze);
+
+        //Gui for drawing a maze automatically
+        JPanel MAutoGen = new JPanel();
+
+
+        JButton autogenbut = new JButton("Generate a Maze!");
+        autogenbut.addActionListener(generatebutton);
+        MAutoGen.add(autogenbut);
+
+        MAutoGen.add(new JLabel("Please Enter Height"));
+        MAutoGen.add(new JTextField(2));
+        MAutoGen.add(new JLabel("Please Enter Width"));
+        MAutoGen.add(new JTextField(2));
+
+
+
+        JButton AutoPath = new JButton("Check Maze Path");
+        AutoPath.addActionListener(generatepath);
+        MAutoGen.add(AutoPath);
+
+        MAutoGen.add(new JTextField("Maze Name"));
+        MAutoGen.add(new JTextField("Author Name"));
+
+        JRadioButton agLogo = new JRadioButton("Auto place logo");
+        agLogo.addActionListener(autoplace);
+        MAutoGen.add(agLogo);
+
+        JButton gplaceLogo = new JButton("Place Image");
+        gplaceLogo.addActionListener(placeimage);
+        MAutoGen.add(gplaceLogo);
+
+        JButton gsaveMaze = new JButton("Save Maze");
+        gsaveMaze.addActionListener(savebutton);
+        MAutoGen.add(gsaveMaze);
+
+
+        //Gui for maze browser
+
+        //table names
+        String[] columnNames = {"Maze Name", "Author", "Difficulty"};
+
+        //temp set of objects for table
+        Object[][] mazelist = {{"maze 1", "maze man", "Difficulty is hard"}, {"John", "Doe", "Rowing",}, {"Sue", "Black", "Knitting"}, {"Jane", "White", "Speed reading",}, {"Joe", "Brown", "Pool"}};
+        JPanel MazeBrowser = new JPanel(new BorderLayout());
+        JTable MazeTable = new JTable(mazelist, columnNames);
+        MazeBrowser.add(MazeTable, BorderLayout.CENTER);
+        MazeBrowser.add(MazeTable.getTableHeader(), BorderLayout.NORTH);
+        JMenuBar mb = new JMenuBar();
+        JMenu eo = new JMenu("Maze Export Options");
+        JMenuItem exporttofile = new JMenuItem("Export Selected to File");
+        JMenuItem exporttodb = new JMenuItem("Export Selected to DataBase");
+        eo.add(exporttofile); eo.add(exporttodb);
+        mb.add(eo);
+        MazeBrowser.add(mb, BorderLayout.SOUTH);
+
+
         pane.add("Draw a Maze from Scratch",MfromScratch);
+        pane.add("Generate a Maze", MAutoGen);
+        pane.add("Maze List", MazeBrowser);
+
         ParentFrame.getContentPane().add(pane);
         ParentFrame.setPreferredSize(new Dimension(450,300));
         ParentFrame.pack();
