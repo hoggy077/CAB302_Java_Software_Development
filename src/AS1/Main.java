a9037245c79a5395534e7b1490b65ca20b79793a
GeorgeN9690298 <n9690298@qut.edu.au>
2022-06-07 16:20:38 +1000

    updates to database class

--- src/AS1/Main.java
+++ src/AS1/Main.java
@@ -1,17 +1,25 @@
 package AS1;
 
 import AS1.AStar.AStNode;
+import AS1.Database.Database;
 import AS1.GUI.MainGUI;
 import AS1.GUI.Menu;
 import AS1.Maze.Maze;
 import AS1.Maze.MazeCell;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws SQLException {
 
         Menu main = new Menu();
 
         main.HomeGUI();
+
+        Database db = new Database();
+
+        db.createTable();
+
 
 
     }
