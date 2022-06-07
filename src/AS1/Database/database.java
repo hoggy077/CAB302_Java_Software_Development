75fe4e9fcbe3d5e683e7fa85ef2081da64941171
GeorgeN9690298 <n9690298@qut.edu.au>
2022-06-07 16:10:37 +1000

    updates to database class

--- src/AS1/Database/database.java
+++ src/AS1/Database/database.java
@@ -1,4 +1,72 @@
 package AS1.Database;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
 public class database {
+
+    private static Connection instance = null;
+
+    public database() throws SQLException {
+
+        Properties props = new Properties();
+        FileInputStream in = null;
+
+        try {
+            in = new FileInputStream("./db.props");
+            props.load(in);
+            in.close();
+
+            String url = props.getProperty("jdbc.url");
+            String username = props.getProperty("jdbc.username");
+            String password = props.getProperty("jbdc.password");
+            String schema = props.getProperty("jdbc.schema");
+
+            // get a connection
+            instance = DriverManager.getConnection(url + "/" + schema, username,
+                    password);
+        } catch (SQLException sqle) {
+            System.err.println(sqle);
+        } catch (FileNotFoundException fnfe) {
+            System.err.println(fnfe);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    /**
+     * Provides global access to the singleton instance of the UrlSet.
+     *
+     * @return a handle to the singleton instance of the UrlSet.
+     */
+    public static Connection getInstance() throws SQLException {
+        if (instance == null) {
+            new database();
+        }
+        return instance;
+    }
+
+    public static final String CREATE_TABLE =
+            "CREATE TABLE IF NOT EXISTS maze ("
+                    + "id INTEGER PRIMARY KEY /*!40101 AUTO_INCREMENT */ NOT NULL UNIQUE,"
+                    + "authorName VARCHAR(30),"
+                    + "mazeName VARCHAR(20),"
+                    + "dateCreated DATE,"
+                    + "dateLastEdited DATE,"
+                    + "image VARBINARY)";
+
+    public void createTable() throws SQLException {
+        Connection connection = database.getInstance();
+        try {
+            Statement st = connection.createStatement();
+            st.execute(CREATE_TABLE);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
--- src/AS1/Main.java
+++ src/AS1/Main.java
@@ -1,17 +1,27 @@
 package AS1;
 
 import AS1.AStar.AStNode;
+import AS1.Database.database;
 import AS1.GUI.MainGUI;
 import AS1.GUI.Menu;
 import AS1.Maze.Maze;
 import AS1.Maze.MazeCell;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws SQLException {
 
         Menu main = new Menu();
 
         main.HomeGUI();
+
+        database db = new database();
+
+        db.createTable();
+
+
+
 
 
     }
